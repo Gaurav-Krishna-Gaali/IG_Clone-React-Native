6553cc1106fa0988e9923d1922ab2ebaec662127
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2022-04-21 00:47:09 +0530

    Stories Done

---
+++ data/users.js
@@ -0,0 +1,32 @@
+export const USERS = [
+  {
+    user: "Gaurav Krishna",
+    image:
+      "https://www.bosshunting.com.au/wp-content/uploads/2019/12/Harvey-Specter-Salary_-What-Would-It-Cost-To-Live-His-Life_.jpg",
+  },
+  {
+    user: "Phineas",
+    image:
+      "https://i.pinimg.com/originals/98/ef/8c/98ef8c6907cfefed086ba1a6847838de.png",
+  },
+  {
+    user: "Ronaldo",
+    image:
+      "https://www.planetsport.com/image-library/square/500/m/manchester-united-cristiano-ronaldo-16-october-2021.jpg",
+  },
+  {
+    user: "Messi",
+    image:
+      "https://icdn.football-espana.net/wp-content/uploads/2020/11/merlin_153612873_5bb119b9-8972-4087-b4fd-371cab8c5ba2-superJumbo.jpg",
+  },
+  {
+    user: "Patrik Jane",
+    image:
+      "https://www.tvinsider.com/wp-content/uploads/images/content/promos/15-08-054.jpg",
+  },
+  {
+    user: "Kick Buttoski",
+    image:
+      "https://w0.peakpx.com/wallpaper/917/929/HD-wallpaper-kick-buttowski-action-cartoon-cool-disney-disneyxd-trending.jpg",
+  },
+];
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import React from "react";
 import Header from "../components/Home/Header";
+import Stories from "../components/Home/Stories";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <Stories />
     </SafeAreaView>
   );
 };
